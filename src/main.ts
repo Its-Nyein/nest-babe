ccb2f82793b4a69dfcddbeb11b00589bdd9d7fd9
nyeinphyoaung <nyeinphyoaung.edu@gmail.com>
2025-03-13 22:33:27 +0630

    chore: nest docs API endpoints with swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './all-exceptions.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+      .setTitle('NestBabee')
+      .setDescription('The best API docs ever:3')
+      .setVersion('1.0')
+      .addTag('cats')
+      .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
   const { httpAdapter} = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
   app.enableCors();
