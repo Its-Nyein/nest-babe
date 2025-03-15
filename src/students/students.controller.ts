8b6a9d9c774d6cd86dab0660f246371e51698536
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2025-03-15 13:43:42 +0630

    chore: add ApiBody and Apiconsumes in swagger

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -11,12 +11,33 @@ import {
 } from '@nestjs/common';
 import { StudentsService } from './students.service';
 import { Prisma } from '@prisma/client';
+import { ApiBody, ApiConsumes } from '@nestjs/swagger';
 
 @Controller('students')
 export class StudentsController {
   constructor(private readonly studentsService: StudentsService) {}
 
   @Post()
+  @ApiConsumes('application/json')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        name: {
+          type: 'string',
+          example: 'John Doe',
+        },
+        email: {
+          type: 'string',
+          example: 'email@gmail.com',
+        },
+        password: {
+          type: 'string',
+          example: 'password',
+        },
+      },
+    },
+  })
   create(@Body() createStudentsDto: Prisma.StudentsCreateInput) {
     return this.studentsService.create(createStudentsDto);
   }
@@ -32,6 +53,17 @@ export class StudentsController {
   }
 
   @Patch(':id')
+  @ApiConsumes('application/json')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        name: { type: 'string', example: 'Updated Name' },
+        email: { type: 'string', example: 'updated.email@example.com' },
+        password: { type: 'string', example: 'updatedpassword' },
+      },
+    },
+  })
   update(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateStudentsDto: Prisma.StudentsUpdateInput,
